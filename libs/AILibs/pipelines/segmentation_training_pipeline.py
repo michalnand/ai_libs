4744d6c5b27529023fd4a1c229b37541512ca993
michalnand <michal.nand@gmail.com>
2026-05-29 20:40:20 +0200

    segmentation pipeline

--- libs/AILibs/pipelines/segmentation_training_pipeline.py
+++ libs/AILibs/pipelines/segmentation_training_pipeline.py
@@ -101,7 +101,7 @@ class SegmentationTrainingPipeline:
             y_batch = y_batch.detach().cpu().numpy()
             y_pred = torch.sigmoid(y_pred).detach().cpu().numpy()
 
-            metric = AILibs.metrics.detection_evaluation( y_batch, y_pred)
+            metric = AILibs.metrics.detection_evaluation(y_batch, y_pred)
             time_stop = time.time()
 
             timing_stats["metric_processing"] = round(time_stop - time_start, 4)
@@ -127,6 +127,7 @@ class SegmentationTrainingPipeline:
             result_log["iou"]       = metric["iou"]
             result_log["f1_score"]  = metric["f1_score"]
             result_log["dice"]      = metric["dice"]
+            result_log["accuracy"]  = metric["accuracy"]
 
             result_log["timing"]    = timing_stats
             result_log["metric"]    = metric
@@ -135,7 +136,7 @@ class SegmentationTrainingPipeline:
             log_str = str(json.dumps(result_log) + "\n")
             f = open(results_path + "/training.log", "a+")
             f.write(log_str)
-            f.close()
+            f.close()   
             
             print(log_str)
 
@@ -147,3 +148,112 @@ class SegmentationTrainingPipeline:
                 saving_version+= 1
                 
                 print("saving done\n\n")
+
+        if self.config.testing_datset is not None:
+            self._testing_model(results_path, self.config.testing_datset, self.config.num_testing_samples)
+
+    def _testing_model(self, results_path, dataset, num_testing_samples):
+
+        print("\n\n\n")
+        print("testing")
+
+        indices      = []
+        y_batch      = []
+        y_pred_batch = []
+
+        timing = []
+
+        self.config.model.eval()
+
+        for n in range(num_testing_samples):
+
+            idx = numpy.random.randint(0, len(dataset))
+            x, y = dataset[idx]
+
+            x_t = torch.from_numpy(x).float().unsqueeze(0).to(self.config.device)
+            y_t = torch.from_numpy(y).float().unsqueeze(0).to(self.config.device)
+
+            time_start = time.time()
+
+            with torch.no_grad():
+                y_pred = self.config.model(x_t)
+                y_pred = torch.sigmoid(y_pred)
+
+            time_stop = time.time()
+
+            timing.append(time_stop - time_start)
+
+            indices.append(idx)
+            y_batch.append(y_t.detach().cpu().numpy())
+            y_pred_batch.append(y_pred.detach().cpu().numpy())  
+
+
+        timing  = numpy.array(timing)
+
+        indices = numpy.array(indices)
+        y_batch = numpy.array(y_batch)
+        y_pred_batch = numpy.array(y_pred_batch)
+
+        metric = AILibs.metrics.detection_evaluation(y_batch, y_pred_batch)
+
+        #outliers = self._find_outliers(indices, y_batch, y_pred_batch)
+
+        # pipeline timing stats
+        timing_stats = {}
+        timing_stats["mean"] = round(timing.mean(), 6)
+        timing_stats["std"]  = round(timing.std(), 6)
+        timing_stats["p68"]  = round(numpy.percentile(timing, 68), 6)
+        timing_stats["p95"]  = round(numpy.percentile(timing, 95), 6)
+        timing_stats["p997"] = round(numpy.percentile(timing, 99.7), 6)
+        timing_stats["max"]  = round(numpy.max(timing), 6)
+
+
+        # predictions values distribution
+        gt_hist, gt_bin_edges = numpy.histogram(y_batch, bins=50)
+        hist, bin_edges = numpy.histogram(y_pred_batch, bins=50)
+
+
+        distribution = {}
+        distribution["gt_hist"]      = self._np_to_list(gt_hist, 5)
+        distribution["gt_bin_edges"] = self._np_to_list(gt_bin_edges, 5)
+        distribution["hist"]         = self._np_to_list(hist, 5)
+        distribution["bin_edges"]    = self._np_to_list(bin_edges, 5)
+        
+
+
+        result_log = {}
+        result_log["num_samples"] = num_testing_samples
+
+        result_log["iou"]       = metric["iou"]
+        result_log["f1_score"]  = metric["f1_score"]
+        result_log["dice"]      = metric["dice"]
+        result_log["accuracy"]  = metric["accuracy"]
+
+        result_log["timing"]    = timing_stats
+        result_log["metric"]    = metric
+        result_log["distribution"]    = distribution
+        result_log["outliers"]  = "none"
+
+
+        log_str = str(json.dumps(result_log) + "\n")
+        f = open(results_path + "/testing.log", "a+")
+        f.write(log_str)
+        f.close()
+
+        print(log_str)
+
+    def _find_outliers(self, indices, y_gt, y_pred, th = 0.5):
+        confidently_wrong = []
+
+        for n in range(len(indices)):
+
+            neg_count = numpy.where(y_pred)
+
+            iou, acc = self._iou_acc(y_gt[n], y_pred[n])
+    
+    def _np_to_list(self, x, dp):
+        result = []
+        for v in x:
+            result.append(round(v.item(), dp))
+
+        return result
